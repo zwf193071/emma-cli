9c4df0eb3f79c93638eedbe74ddaf202487b1443
zhuwenfang <zhuwenfang@inspur.com>
2019-04-19 17:32:57 +0800

    模仿vue 2.0的emma cli

--- utils/index.js
+++ utils/index.js
@@ -1,22 +1,6 @@
 [
-    'env',
-    'loadCommand',
     'logger',
     'spinner',
-    'validate',
-    'exit',
-    'rcPath',
-    'request',
-    'installDeps',
-    'module',
-    'normalizeFilePaths',
-    'injectImportsAndOptions',
-    'sortObject',
-    'getVersions',
-    'pluginResolution',
-    'mergeDeps',
-    'extendJSConfig',
-    'stringifyJS'
   ].forEach(m => {
     Object.assign(exports, require(`./lib/${m}`))
   })
--- utils/lib/stringifyJS.js
+++
@@ -1,9 +0,0 @@
-exports.stringifyJS = (value) => {
-  const stringify = require('javascript-stringify')
-  return stringify(value, (val, indent, stringify) => {
-    if (val && val.__expression) {
-      return val.__expression
-    }
-    return stringify(val)
-  }, 2)
-}
